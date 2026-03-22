86cc5c7fa8dd03843fe8ad1dbe287211d2360779
djonei <69280288+djonei@users.noreply.github.com>
2026-03-22 10:27:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/candle-cart-creator/",
   server: {
     host: "::",
     port: 8080,
